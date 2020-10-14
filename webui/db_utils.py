a93bd717c51c765204b14032bc5d7dd38182c404
tobiasjcat <paul@croft.club>
2020-10-13 18:30:42 -0700

    Completed subtitle parser and indexer

--- webui/db_utils.py
+++ webui/db_utils.py
@@ -8,8 +8,15 @@ import os
 from pprint import pformat, pprint
 import sqlite3
 import string
+import sys
+
+def fastwrite(instr):
+    sys.stdout.write(instr)
+    sys.stdout.flush()
+
 
 conn = sqlite3.connect("frink.db")
+# conn = sqlite3.connect(":memory:")
 c = conn.cursor()
 
 
@@ -17,16 +24,32 @@ vfiletypes = ["mkv","avi","mp4"]
 
 def insert_subs(infilm, insubtrack):
     # print("Inserting subs for {} from {}".format(infilm, insubtrack))
+    fid = c.execute("SELECT rowid FROM films WHERE filepath == ?", (infilm, )).fetchone()[0]
+
+    # print(fid, infilm)
     full_text = None
     with open(insubtrack, 'rb') as subfile:
         full_bytes = subfile.read()
         full_text = codecs.decode(full_bytes,errors='ignore')#Thanks mom-python3. Reading binary _should_ be more complicated
         full_text = ''.join(filter(lambda x:x in string.printable, full_text))
     sub_chunks = list(filter(lambda x:x.strip(), full_text.split('\r\n\r\n')))
-    print("for {} I found {} chunks".format( infilm.rsplit('/',1)[1], len(sub_chunks) ) ) 
+    # print("for {} I found {} chunks".format( infilm.rsplit('/',1)[1], len(sub_chunks) ) ) 
     if len(sub_chunks) < 5:
         return
-    # print(sub_chunks[-5])
+    vals_to_insert = []
+    for chunk in sub_chunks:
+        temp = list(filter(None, chunk.split('\r\n',2)))
+        if len(temp) < 3:#badly formatted chunk
+            # pprint(temp)
+            continue
+        st, et = '',''
+        st,et = temp[1].split("-->")
+        st, et = st.strip(), et.strip()
+        newval = (fid,st,et,temp[2].strip(), )
+        vals_to_insert.append(newval)
+    # pprint(vals_to_insert)
+    c.executemany("INSERT INTO subtitles VALUES (?, ?, ?, ?)", vals_to_insert)
+    # conn.commit()
 
 def build_tables():
 
@@ -48,20 +71,41 @@ def build_tables():
                     temp = sin[:-3]
                     for vft in vfiletypes:
                         film_abs = "{}/{}{}".format(sf,temp,vft)
-                        if temp + vft in subfolder_inodes:
+                        if (temp + vft) in subfolder_inodes:
+                            # print(temp + vft)
+                            # pprint(subfolder_inodes)
                             c.execute("INSERT INTO films VALUES (?)", (film_abs, ) )
+                            insert_subs(film_abs, "{}/{}".format(sf, sin))
                             break
-                    insert_subs(film_abs, "{}/{}".format(sf, sin))
     conn.commit()
     return retval
 
 
 
 def main():
+    fastwrite("Dropping old data...")
     c.execute("DROP TABLE IF EXISTS films")
     c.execute("DROP TABLE IF EXISTS subtitles")
+    fastwrite("Done\n")
 
+    fastwrite("Building new data...")
     build_tables()
+    fastwrite("Done\n")
+
+    fastwrite("Indexing...")
+    c.execute("CREATE INDEX fidx ON films(filepath)")
+    c.execute("CREATE INDEX sindex1 ON subtitles(film_id)")
+    c.execute("CREATE INDEX sindex2 ON subtitles(payload)")
+    fastwrite("Done\n")
+
+    fastwrite("Vacuuming...")
+    c.execute("VACUUM")
+    fastwrite("Done\n")
+
+    nfilms = c.execute("SELECT Count(rowid) FROM films").fetchone()[0]
+    nsubs = c.execute("SELECT Count(rowid) FROM subtitles").fetchone()[0]
+
+    print("Built tables for {} films and {} subtitles".format(nfilms, nsubs))
 
     return 0
 
